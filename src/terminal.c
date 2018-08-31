ad220ba1f301d5293c8ed49464a094b6d94f3df8
Ellen Marie Dash <me@duckie.co>
2018-08-30 23:31:07 -0400

    ~~~~ DOCUMENTATION ~~~

--- src/terminal.c
+++ src/terminal.c
@@ -16,6 +16,14 @@ void eli_terminal_enable_raw_mode()
     tcgetattr(STDIN_FILENO, &original_termios);
     atexit(eli_terminal_disable_raw_mode);
     struct termios raw = original_termios;
+
+    // Actually enter raw mode.
     cfmakeraw(&raw);
+
+    // Allow read() to return as soon as a character is typed.
+    raw.c_cc[VMIN] = 0;
+    // Force read() to time out after 1/10th of a second.
+    raw.c_cc[VTIME] = 1;
+
     tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
 }
